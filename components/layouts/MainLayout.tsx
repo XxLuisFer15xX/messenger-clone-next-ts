d55f5acd1821e0510b3ef31906e048a21781d4ef
XxLuisFer15xX <luisfer.sm15@gmail.com>
2023-05-21 10:27:48 -0600

    :wrench: Configuring the initial files.

---
+++ components/layouts/MainLayout.tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+import styles from '@styles/MainLayout.module.css'
+
+export const metada = {
+  title: 'Messenger Clone',
+  description: 'Messenger Clone',
+}
+export const MainLayout = ({
+  children,
+  description,
+  imageFullUrl,
+  title,
+}: {
+  children: React.ReactNode
+  title: string
+  description: string
+  imageFullUrl?: string
+}) => {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {imageFullUrl && <meta name="og:image" content={imageFullUrl} />}
+      </Head>
+      <main className={`flex flex-col ${styles['container-main']}`}>
+        {children}
+      </main>
+    </div>
+  )
+}
